2aa923c6a665a02a9465e99b4cb98f8671978f52
AlisonB319 <alburgess@microsoft.com>
2023-04-20 14:40:08 -0700

    revert back to \n

--- pkg/agent/utils.go
+++ pkg/agent/utils.go
@@ -258,7 +258,7 @@ func IsKubernetesVersionGe(actualVersion, version string) bool {
 
 func getCustomDataFromJSON(jsonStr string) string {
 	var customDataObj map[string]string
-	jsonStr = strings.ReplaceAll(jsonStr, "\r", "\r\n")
+	jsonStr = strings.ReplaceAll(jsonStr, "\r", "\n")
 	jsonStr = strings.ReplaceAll(jsonStr, "\n", "\\n")
 	jsonStr = strings.ReplaceAll(jsonStr, "\t", "\\t")
 	err := json.Unmarshal([]byte(jsonStr), &customDataObj)
